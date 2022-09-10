ec7e642f97685956b7916f281d052fb61432edfc
David <david-bitner@hotmail.com>
2022-09-10 12:30:01 -0300

    Update style.css
    Multiple minor changes and added more projects section

--- src/js/script.js
+++ src/js/script.js
@@ -15,30 +15,32 @@ class UI {
       return;
     }
 
-    const background = document.querySelector("body");
+    const body = document.querySelector("body");
+
+    body.style.color = "white";
 
     if (projectId == "project1") {
-      background.style.backgroundColor = "#008f11";
+      body.style.backgroundColor = "#008f11";
     }
     if (projectId == "project2") {
-      background.style.backgroundColor = "#d6a283";
+      body.style.backgroundColor = "#d6a283";
     }
     if (projectId == "project3") {
-      background.style.backgroundColor = "#e2aada";
+      body.style.backgroundColor = "#e2aada";
     }
     if (projectId == "project4") {
-      background.style.backgroundColor = "#808080";
+      body.style.backgroundColor = "#606060";
     }
     if (projectId == "project5") {
-      background.style.backgroundColor = "#000533";
+      body.style.backgroundColor = "#000533";
     }
     if (projectId == "project6") {
-      background.style.backgroundColor = "#212121";
+      body.style.backgroundColor = "#212121";
     }
   }
 
   static resetBackground() {
-    document.querySelector(`body`).style.backgroundColor = "white";
+    document.body.style.backgroundColor = "#7d7d7d";
   }
 
   static openPopup(projectId) {
@@ -47,20 +49,41 @@ class UI {
     }
 
     const popup = document.querySelector(`.popup`);
+    const overlay = document.querySelector(`#overlay-popup`);
 
-    document.querySelector(`.overlay`).style.visibility = "visible";
-    document.querySelector(`.overlay`).style.opacity = "1";
+    overlay.style.visibility = "visible";
+    overlay.style.opacity = "1";
     popup.style.transform = "translateY(0)";
+    document.body.style.overflow = "hidden";
   }
 
   static closePopup() {
     const popup = document.querySelector(`.popup`);
-    const overlay = document.querySelector(`.overlay`);
+    const overlay = document.querySelector(`#overlay-popup`);
+
+    document.body.style.overflow = "visible";
+    overlay.style.visibility = "hidden";
+    overlay.style.opacity = "0";
+    popup.style.transform = "translateY(100rem)";
+    popup.scrollTop = 0;
+  }
+
+  static openModal() {
+    const modal = document.querySelector(`.modal`);
+    const overlay = document.querySelector(`#overlay-modal`);
+
+    overlay.style.visibility = "visible";
+    overlay.style.opacity = "1";
+    modal.style.transform = "scale(1)";
+  }
+
+  static closeModal() {
+    const modal = document.querySelector(`.modal`);
+    const overlay = document.querySelector(`#overlay-modal`);
 
     overlay.style.visibility = "hidden";
     overlay.style.opacity = "0";
-    popup.style.transform = "translateY(50rem)";
-    popup.scrollTop = 0;
+    modal.style.transform = "scale(0.8)";
   }
 }
 
@@ -78,4 +101,12 @@ window.addEventListener("click", (e) => {
   if (e.target == document.querySelector(`.overlay`)) {
     UI.closePopup();
   }
+
+  if (e.target == document.querySelector(`#overlay-modal`)) {
+    UI.closeModal();
+  }
 });
+
+document
+  .querySelector(`#btn-more-projects`)
+  .addEventListener("click", UI.openModal);
